465f5baee23997a1512bed619356691bf5b7837c
jinjianpacong <1826743393@qq.com>
2022-02-24 07:54:45 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("666");
         System.out.println("777");
+        System.out.println("888");
     }
 }
