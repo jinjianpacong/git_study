56bdcf960e18fcceedc8916ae31ddff86d63dd30
jinjianpacong <1826743393@qq.com>
2022-02-24 07:52:01 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 2014080902211
+ * @Date: 2022/02/24/7:48
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("666");
+    }
+}
