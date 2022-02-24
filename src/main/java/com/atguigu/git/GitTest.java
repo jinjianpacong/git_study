3d074637601b3bdc08e0bdcff44dc4b906a5928e
jinjianpacong <74414374+jinjianpacong@users.noreply.github.com>
2022-02-24 11:07:33 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,11 +10,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("666");
-        System.out.println("777");
-        System.out.println("888");
-        System.out.println("999");
-        System.out.println("101010");
-        System.out.println("111111");
         System.out.println("hot-fix test");
     }
 }
